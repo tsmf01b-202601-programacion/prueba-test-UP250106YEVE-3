86b9667fd846342b39afa9fe5f87555d5350738e
UP250106-YEVE <up250106@alumnos.upa.edu.mx>
2026-04-09 17:59:19 +0000

    examen

--- examen.py
+++ examen.py
@@ -11,53 +11,62 @@ if problema == 1:
     # Problema 1: Área de un círculo con radio 15.
     # Fórmula: Area = 3.1416 * r^2
     r = 15
-    # Tu código aquí
+    area = 3.1416 * r **2
+    print(area)
 
 elif problema == 2:
     # Problema 2: Genera una clave de producto (concatenar) con código y lote.
     # Resultado esperado: "prod-88-loteb" (todo en minúsculas).
-    codigo = 88
+    codigo = '88-'
     lote = "LoteB"
-    # Tu código aquí
-
+    prod = 'prod-'
+    clave = prod.lower() +  codigo + lote.lower()
+    print(clave)
 elif problema == 3:
     # Problema 3: Verifica si el carácter '@' está en el correo dado.
     email = "usuario.upa.edu.mx"
-    # Tu código aquí
+    resultado = '@' in email
+    print(resultado)
 
 elif problema == 4:
     # Problema 4: Convierte todo el texto a MAYÚSCULAS.
     aviso = "el examen termina pronto"
-    # Tu código aquí
+    print(aviso.upper())
 
 elif problema == 5:
     # Problema 5: Convierte el string "150.50" a float y luego a entero.
     # Imprime ambos resultados en una sola línea.
     dato = "150.50"
-    # Tu código aquí
+    dato_float = float(dato)
+    dato_int = int(dato_float)
 
 elif problema == 6:
     # Problema 6: Conversión de Temperatura (Celsius a Fahrenheit).
     # C=30. Fórmula: F = (C * 1.8) + 32
     celsius = 30
-    # Tu código aquí
+    fahrenheit = (celsius * 1.8) + 32
+    print(fahrenheit)
 
 elif problema == 7:
     # Problema 7: Extrae los primeros 5 caracteres de la cadena.
     frase = "Programación en Python"
-    # Tu código aquí
+    print(frase[:5])
 
 elif problema == 8:
     # Problema 8: Calcula la Densidad de un objeto.
     # Masa: 500kg, Volumen: 2m^3. Fórmula: d = m / v
     m = 500
     v = 2
-    # Tu código aquí
+    densidad = m / v
+    print(densidad)
 
 elif problema == 9:
     # Problema 9: Determina si un número es negativo.
     numero = -15
-    # Tu código aquí
+    if numero < 0:
+        print("es negativo")
+    else:
+        print("no es negativo")
 
 elif problema == 10:
     # Problema 10: Calcula la Energía Potencial.
@@ -65,7 +74,8 @@ elif problema == 10:
     m = 5
     h = 10
     g = 9.81
-    # Tu código aquí
+    energia_potencial = m * g * h
+    print(energia_potencial)
 
 else:
     print("Ingresa un número entre 1 y 10.")
